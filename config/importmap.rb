8ca4565c7312028181cdbdc2209a5653d83776b4
bby-kanta <kan.kan19980928@gmail.com>
2025-08-11 01:31:58 +0900

    Add `fighter_features` to import map and application.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.200
 # pin_all_from "app/javascript/channels", under: "channels"
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
+pin "fighter_features", to: "fighter_features.js"
