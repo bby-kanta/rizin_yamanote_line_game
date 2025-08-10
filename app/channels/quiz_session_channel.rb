808df5475586eb887eaf8ffeb9a343837a94c8e4
bby-kanta <kan.kan19980928@gmail.com>
2025-08-10 21:07:36 +0900

    Revert "Add user connection and disconnection notifications; implement ping mechanism for real-time connection status"
    This reverts commit 39a2f5fbe3f36f4b1ae55ae3e00ece0f1e60e358.

--- app/channels/quiz_session_channel.rb
+++ app/channels/quiz_session_channel.rb
@@ -2,32 +2,9 @@ class QuizSessionChannel < ApplicationCable::Channel
   def subscribed
     quiz_session = QuizSession.find(params[:quiz_session_id])
     stream_from "quiz_session_#{quiz_session.id}"
-    
-    # 接続時に他の参加者に通知
-    ActionCable.server.broadcast("quiz_session_#{quiz_session.id}", {
-      type: 'user_connected',
-      user_id: current_user.id,
-      user_name: current_user.name
-    })
   end
 
   def unsubscribed
-    # 切断時に他の参加者に通知
-    quiz_session_id = params[:quiz_session_id]
-    ActionCable.server.broadcast("quiz_session_#{quiz_session_id}", {
-      type: 'user_disconnected', 
-      user_id: current_user.id,
-      user_name: current_user.name
-    })
-  end
-  
-  # クライアントからのping受信
-  def ping(data)
-    # pongを返す
-    ActionCable.server.broadcast("quiz_session_#{params[:quiz_session_id]}", {
-      type: 'pong',
-      user_id: current_user.id,
-      timestamp: data['timestamp']
-    })
+    # Any cleanup needed when channel is unsubscribed
   end
 end
