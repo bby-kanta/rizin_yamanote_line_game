5046f8afc95645d162014993d5e2de5fabb0b73d
bby-kanta <kan.kan19980928@gmail.com>
2025-08-10 19:26:59 +0900

    Remove MySQL-generated `schema.rb` for PostgreSQL compatibility in Render build script.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,8 @@ set -o errexit
 
 bundle install
 bundle exec rake assets:precompile
+
+# Remove MySQL-generated schema.rb for PostgreSQL compatibility
+rm -f db/schema.rb
+
 bundle exec rake db:migrate
